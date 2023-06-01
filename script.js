e80c6064f7f4393126cd7343cb0377f78edc459e
Panda <106486268+C0ding-Panda@users.noreply.github.com>
2023-06-01 17:48:56 +0200

    v0.02
    sounds, mobile, smoother

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 
+const button = document.querySelector('button');
+button.addEventListener('click', requestAutoplayPermission);
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
@@ -9,12 +11,29 @@ let ballDX = 3;
 let ballDY = -3;
 let ballColor = "#000"; // Added this line to set initial ball color
 
-let paddleHeight = 75;
+let paddleHeight = 90;
 let paddleWidth = 10;
 let playerPaddleY = (canvas.height - paddleHeight) / 2;
 
 let totalTouches = 0;
 let touchesSinceLoss = 0;
+let highScore = 0;
+
+const pong_sound = document.getElementById("pong-sound");
+const ding_sound = document.getElementById("ding-sound");
+let allowSound = false;
+async function requestAutoplayPermission() {
+  try {
+    await navigator.mediaDevices.requestPermission('audio');
+  } catch (error) {
+  }
+}
+function requestAutoplayPermission() {
+  allowSound = true;
+}
+function deactivateAudio() {
+  allowSound = false;
+}
 
 function drawBall() {
   ctx.beginPath();
@@ -27,27 +46,33 @@ function drawBall() {
 function drawPaddle(x, y) {
   ctx.beginPath();
   ctx.rect(x, y, paddleWidth, paddleHeight);
-  ctx.fillStyle = "#000"; // Changed this line to set paddle color to black
+  ctx.fillStyle = "#0000ff"; // Changed this line to set paddle color to black
   ctx.fill();
   ctx.closePath();
 }
 function addPaddleHeight() {
-paddleHeight = 100;
+  paddleHeight = 120;
 }
 
 function subtractPaddleHeight() {
-paddleHeight = 40;
+  paddleHeight = 60;
 }
 function resetPaddleHeight() {
-paddleHeight = 75;
+  paddleHeight = 90;
 }
+
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
-  drawPaddle(0, playerPaddleY);
+  drawPaddle(0, playerPaddleY,);
 
   if (ballY + ballDY < ballRadius || ballY + ballDY > canvas.height - ballRadius) { 
     ballDY = -ballDY; 
+    if (allowSound == true) {
+      ding_sound.currentTime = 0;
+      ding_sound.pause();
+      ding_sound.play();  
+    }
     function getRandomColor() {
 const letters = "0123456789ABCDEF";
 let color = "#";
@@ -57,17 +82,23 @@ color += letters[Math.floor(Math.random() * 16)];
 return color;
 }
 
-     ballColor = getRandomColor();
+    ballColor = getRandomColor();
   }
 
-  if (ballX + ballDX < ballRadius + paddleWidth) { // Left wall collision
+  if (ballX + ballDX < ballRadius + paddleWidth) { // paddle collision
     if (ballY + ballDY > playerPaddleY && ballY + ballDY < playerPaddleY + paddleHeight) {
       ballDX = -ballDX;
       ballDY += 0.5;
       totalTouches++;
       touchesSinceLoss++;
-      ballDX *= 1.2; // Increase ball speed
-     ballDY *= 1.2;
+      if (totalTouches > highScore) {
+        highScore = totalTouches;
+      }
+      ballDX *= 1.07; // Increase ball speed
+      ballDY *= 1.07;
+      if (allowSound == true) {
+        pong_sound.play();
+      }
     ballColor = '#000';
     } else {
       alert("You lost!/Du hast verloren! Total touches/Berührungen: " + totalTouches);
@@ -80,6 +111,11 @@ return color;
     }
   } else if (ballX + ballDX > canvas.width - ballRadius) { // Right wall collision
     ballDX = -ballDX;
+    if (allowSound == true) {
+      ding_sound.currentTime = 0;
+      ding_sound.pause();
+      ding_sound.play();
+    }
     function getRandomColor() {
     const letters = "0123456789ABCDEF";
     let color = "#";
@@ -89,22 +125,65 @@ return color;
     return color;
     } 
 
-     ballColor = getRandomColor();
+    ballColor = getRandomColor();
   }
 
   ballX += ballDX;
   ballY += ballDY;
 document.getElementById("total-touches").textContent = totalTouches.toString();
 document.getElementById("touches-since-loss").textContent = touchesSinceLoss.toString();
+document.getElementById("high-score").textContent = highScore.toString();
 }
+let moveUp = false;
+let moveDown = false;
 
 document.addEventListener("keydown", e => {
-  if (e.keyCode == 38) { // up arrow
-    playerPaddleY -= 15;
-  } else if (e.keyCode == 40) { // down arrow
-    playerPaddleY += 15;
+  if (e.key === "u") {
+    moveUp = true;
+  } else if (e.key === "d") {
+    moveDown = true;
   }
 });
+
+document.addEventListener("keyup", e => {
+  if (e.key === "u") {
+    moveUp = false;
+  } else if (e.key === "d") {
+    moveDown = false;
+  }
+});
+
+
+document.getElementById("left-btn").addEventListener("touchstart", (e) => {
+  e.preventDefault();
+  moveUp = true;
+});
+
+document.getElementById("left-btn").addEventListener("touchend", (e) => {
+  e.preventDefault();
+  moveUp = false;
+});
+
+document.getElementById("right-btn").addEventListener("touchstart", (e) => {
+  e.preventDefault();
+  moveDown = true;
+});
+
+document.getElementById("right-btn").addEventListener("touchend", (e) => {
+  e.preventDefault();
+  moveDown = false;
+});
+
+
+setInterval(() => {
+  if (moveUp && playerPaddleY > 0) { // added check to prevent moving paddle off the top of the canvas
+    playerPaddleY -= 5;
+  }
+  if (moveDown && playerPaddleY + paddleHeight < canvas.height) { // added check to prevent moving paddle off the bottom of the canvas
+    playerPaddleY += 5;
+  }
+}, 10);
+
 
 setInterval(draw, 10);
 
